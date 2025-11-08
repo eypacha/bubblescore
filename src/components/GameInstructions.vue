443565de1aa618c92ea00c5cb7e8da305dc66113
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-08 13:48:13 -0300

    Update game instructions to clarify bubble fusion mechanics

--- src/components/GameInstructions.vue
+++ src/components/GameInstructions.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center mt-4">
     <p class="text-gray-600 text-sm mb-2">
-      Make two bubbles collide whose sum is a multiple of 10 to fuse them.
+      Click on two bubbles whose sum is a multiple of 10 to fuse them.
     </p>
   </div>
 </template>
