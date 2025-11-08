739cabc4277bd9bcd051b3f35ed80eefa6467160
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-07 21:36:44 -0300

    Remove lightenColor and darkenColor methods from ColorManager

--- src/game/color-manager.js
+++ src/game/color-manager.js
@@ -84,25 +84,4 @@ export class ColorManager {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
   }
 
-  lightenColor(color, percent) {
-    const num = parseInt(color.replace("#", ""), 16)
-    const amt = Math.round(2.55 * percent)
-    const R = (num >> 16) + amt
-    const G = (num >> 8 & 0x00FF) + amt
-    const B = (num & 0x0000FF) + amt
-    return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
-      (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 +
-      (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1)
-  }
-
-  darkenColor(color, percent) {
-    const num = parseInt(color.replace("#", ""), 16)
-    const amt = Math.round(2.55 * percent)
-    const R = (num >> 16) - amt
-    const G = (num >> 8 & 0x00FF) - amt
-    const B = (num & 0x0000FF) - amt
-    return "#" + (0x1000000 + (R > 255 ? 255 : R < 0 ? 0 : R) * 0x10000 +
-      (G > 255 ? 255 : G < 0 ? 0 : G) * 0x100 +
-      (B > 255 ? 255 : B < 0 ? 0 : B)).toString(16).slice(1)
-  }
 }
